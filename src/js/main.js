ab51ebf2f271f8bf1520fb8588934dc1239fafdb
Georgiy Filippov <g.filippov777@gmail.com>
2021-03-14 09:47:46 +0500

    add handler to 'window'

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,41 @@
 'use strict';
 
-const tabs = document.querySelectorAll('.tabcontent');
-const tabItems = document.querySelectorAll('.tabheader__item');
-const tabHeader = document.querySelector('.tabheader__items');
+window.addEventListener('DOMContentLoaded', () => {
+    // tabs
+    const tabs = document.querySelectorAll('.tabcontent');
+    const tabItems = document.querySelectorAll('.tabheader__item');
+    const tabHeader = document.querySelector('.tabheader__items');
 
-const hideTabs = () => {
-    tabs.forEach((item) => {
-        item.classList.add('hide');
-        item.classList.remove('show', 'fade');
-    });
-
-    tabItems.forEach((item) => {
-        item.classList.remove('tabheader__item_active');
-    });
-};
-
-const showTabs = (i = 0) => {
-    tabs[i].classList.add('show', 'fade');
-    tabs[i].classList.remove('hide');
-    tabItems[i].classList.add('tabheader__item_active');
-};
-
-hideTabs();
-showTabs();
-
-tabHeader.addEventListener('click', (evt) => {
-    const target = evt.target;
-
-    if (target && target.matches('.tabheader__item')) {
-        tabItems.forEach((item, i) => {
-            if (target == item) {
-                hideTabs();
-                showTabs(i);
-            }
+    const hideTabs = () => {
+        tabs.forEach((item) => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
-    }
+
+        tabItems.forEach((item) => {
+            item.classList.remove('tabheader__item_active');
+        });
+    };
+
+    const showTabs = (i = 0) => {
+        tabs[i].classList.add('show', 'fade');
+        tabs[i].classList.remove('hide');
+        tabItems[i].classList.add('tabheader__item_active');
+    };
+
+    hideTabs();
+    showTabs();
+
+    tabHeader.addEventListener('click', (evt) => {
+        const target = evt.target;
+
+        if (target && target.matches('.tabheader__item')) {
+            tabItems.forEach((item, i) => {
+                if (target == item) {
+                    hideTabs();
+                    showTabs(i);
+                }
+            });
+        }
+    });
 });
